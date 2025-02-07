b1336681a28da92f46c18c48dcbfb8b259acbd3b
Serhii Alieksieienko <in3ruder@hotmail.com>
2025-02-07 14:13:36 +0200

    hw-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,18 @@
-function checkForSpam(message) {
-  const blackListWord1 = "sale";
-  const blackListWord2 = "spam";
-  const normSpam = message.toLowerCase();
-  return normSpam.includes(blackListWord1) || normSpam.includes(blackListWord2);
+
+function filterArray(numbers, value) {
+  let filteredArray = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      filteredArray.push(numbers[i]);
+    }
+  }
+
+  return filteredArray;
 }
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
